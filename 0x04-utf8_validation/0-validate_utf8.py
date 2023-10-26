2dc86c8b1f2fa020c2a16e63a1e8cb8fa53f171b
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-26 14:12:10 +0100

    feat: :sparkles: utf validaton

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data (list): A list of integers representing the bytes of a UTF-8 encoded character sequence.
+
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, else return False.
+    """
+
+    bytes_to_follow = 0
+
+    for num in data:
+        bin_rep = format(num, "08b")
+
+        if bytes_to_follow == 0:
+            if bin_rep.startswith("0"):
+                bytes_to_follow = 0
+            elif bin_rep.startswith("110"):
+                bytes_to_follow = 1
+            elif bin_rep.startswith("1110"):
+                bytes_to_follow = 2
+            elif bin_rep.startswith("11110"):
+                bytes_to_follow = 3
+            else:
+                return False
+        elif bin_rep.startswith("10"):
+            bytes_to_follow -= 1
+
+        else:
+            return False
+    return bytes_to_follow == 0
