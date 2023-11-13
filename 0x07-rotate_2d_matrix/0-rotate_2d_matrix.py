eb492d147a46536bd090cf194c1b991f931ddb53
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-13 09:18:32 +0100

    feat: :sparkles: rotating a 2D Matrix 90 degress

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+def rotate_2d_matrix(matrix):
+    # sourcery skip: use-itertools-product
+    if not matrix:
+        return None
+    # ensure matrix is a square matrix
+    if len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+    # ensure matrix contains only integers or floats
+    for row in matrix:
+        for col in row:
+            if not isinstance(col, (int, float)):
+                raise ValueError("matrix must contain only integers or floats")
+    N = len(matrix)
+    # transpose the matrix
+    for row in range(N):
+        for col in range(row, N):
+            matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]  # noqa: E501
+    # reverse the matrix
+    for row in range((N // 2)):
+        for col in range(N):
+            matrix[col][row], matrix[col][N - 1 - row] = matrix[col][N - 1 - row], matrix[col][row]  # noqa: E501
