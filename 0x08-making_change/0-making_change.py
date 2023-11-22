721925ede736fed1099e73c12d43403f4412eb02
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-22 19:51:20 +0100

    fix: :bug: i hope this is much clever

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -23,11 +23,17 @@ def makeChange(coins, total):
         return -1
 
     # Sort coins in descending order
+    # it makes more sense to take the largest coins first
+    # and reduce the total as much as possible
     coins.sort(reverse=True)
 
     # Use a greedy approach to pick the largest coins first
     count = 0
     for coin in coins:
+        # Reduce the total by the current coin
+        # we know at first iteration that the first coin will be the largest
+        # this operation should be done as long as total is greater than
+        # the current largest coin in the list
         while total >= coin:
             total -= coin
             count += 1
@@ -35,18 +41,24 @@ def makeChange(coins, total):
     # If total is reduced to 0, return the count
     if total == 0:
         return count
+    else:
+        return -1
 
     # Initialize a list to store the minimum number of coins
     # needed for each amount
-    dp = [float("inf")] * (total + 1)
+    # total has been decresed to some value greater than 0
+    # we create an array of size total + 1 and load it with
+    # [inf, inf, inf, inf, ...]
+    # assingn it to dp - dynamic programming
+    # dp = [float("inf")] * (total + 1)
 
-    # Base case: 0 coins needed to make change for 0
-    dp[0] = 0
+    # # Base case: 0 coins needed to make change for 0
+    # dp[0] = 0
 
-    # Iterate through each coin and update the minimum number of coins needed
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    # # Iterate through each coin and update the minimum number of coins needed
+    # for coin in coins:
+    #     for amount in range(coin, total + 1):
+    #         dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    # If dp[total] is still set to infinity, it means the total cannot be met
-    return dp[total] if dp[total] != float("inf") else -1
+    # # If dp[total] is still set to infinity, it means the total cannot be met
+    # return dp[total] if dp[total] != float("inf") else -1
