beaf3f272108684c6374777552f26a4d4db10391
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-27 12:22:46 +0100

    feat: :sparkles: calculating the perimeter of an island base on the grip properties

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" 0-island_perimeter.py """
+
+def island_perimeter(grid: list[list[int]]) -> int:
+    """        
+    Calculate the perimeter of an island described by a grid.
+    
+    Args:
+        grid (list[list[int]]): A 2D grid representing the island, where 1 represents land and 0 represents water. # noqa: E501
+    
+    Returns:
+        int: The perimeter of the island.
+    """    
+    # initialize premeter = 0
+    # perimeter of a square = 4 * side length === 4 * 1 === 4
+    perimeter = 0
+    for rows in range(len(grid)):
+        for cols in range(len(grid[rows])):
+            if grid[rows][cols] == 1:
+                perimeter += 4
+                # check if the block above is land
+                if rows > 0 and grid[rows - 1][cols] == 1:
+                    perimeter -= 2 # subtract 2 because they share the same edge as the block above # noqa: E501
+                # check if the block before is land
+                if cols > 0 and grid[rows][cols - 1] == 1:
+                    perimeter -= 2 # subtract 2 because they share the same edge as the block before # noqa: E501
+    return perimeter
