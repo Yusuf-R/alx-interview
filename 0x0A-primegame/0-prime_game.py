4b8943693f8960c4f97ad7c69154a703b44fd884
Yusuf-R <y.abdulwasiu@gmail.com>
2023-12-08 17:30:14 +0100

    fix: :bug: check the simple list for basic prime

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -14,7 +14,7 @@ def check_prime(n):
     """
     if n < 2:
         return False
-    if n in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61]:  # noqa: E501
+    if n in [2, 3, 5]:
         return True
     # Check if n is divisible by any number from 2 to the square root of n.
     # If it is, then n is not prime.
